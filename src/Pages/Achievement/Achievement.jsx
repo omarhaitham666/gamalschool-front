f1a7f6ee226c752f11628ceeb3d61317a1b122dd
omarhaitham666 <oh2278943@gmail.com>
2024-10-07 04:19:11 +0300

    first commit

--- src/Pages/Achievement/Achievement.jsx
+++ src/Pages/Achievement/Achievement.jsx
@@ -1,74 +1,49 @@
+import { useEffect, useState } from "react";
 import "./AchievementStyle.css";
+
 function Achievement() {
+  const [achievements, setAchievements] = useState([]);
+  useEffect(() => {
+        const fetchAchievements = async () => {
+          try {
+            const response = await fetch('/api/achievements');
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            const data = await response.json();
+            setAchievements(data);
+          } catch (error) {
+            console.error("There was a problem with the fetch operation:", error);
+          }
+        };
+    
+        fetchAchievements();
+      }, []);
+
   return (
-    <>
-    <div className="body">
-    <div id="card-idea">
-      <div className="wrapper">
-        <div className="box-area">
-          <div className="box">
-            <img src="images/images.jpeg" alt="" />
-            <div className="overlay">
-              <h3> عمر هيثم</h3>
-              <p>هيثم اوائل الطلبة </p>
-            </div>
-          </div>
-          <div className="box">
-            <img src="images/images.jpeg" alt="" />
-            <div className="overlay">
-              <h3> عمر هيثم</h3>
-              <p>عمر هيثم اوائل الطلبة</p>
-            </div>
-          </div>
-          <div className="box">
-            <img src="images/images.jpeg" alt="" />
-            <div className="overlay">
-              <h3> عمر هيثم</h3>
-              <p>عمر هيثم اوائل الطلبة</p>
-            </div>
-          </div>
-          <div className="box">
-            <img src="images/images.jpeg" alt="" />
-            <div className="overlay">
-              <h3> عمر هيثم</h3>
-              <p>  عمر هيثم اوائل الطلبة</p>
-            </div>
-          </div>
-          <div className="box">
-            <img src="images/images.jpeg" alt="" />
-            <div className="overlay">
-              <h3> عمر هيثم</h3>
-              <p>عمر هيثم اوائل الطلبة</p>
-            </div>
-          </div>
-          <div className="box">
-            <img src="images/images.jpeg" alt="" />
-            <div className="overlay">
-              <h3> عمر هيثم</h3>
-              <p>عمر هيثم اوائل الطلبة </p>
-            </div>
-          </div>
-          <div className="box">
-            <img src="images/images.jpeg" alt="" />
-            <div className="overlay">
-              <h3> عمر هيثم</h3>
-              <p>  عمر هيثم اوائل الطلبة </p>
-            </div>
-          </div>
-          <div className="box">
-            <img src="images/images.jpeg" alt="" />
-            <div className="overlay">
-              <h3> عمر هيثم</h3>
-              <p> عمر هيثم اوائل الطلبة</p>
-            </div>
+    <div className="body h-[100%]">
+      <div id="card-idea">
+        <div className="wrapper">
+          <div className="box-area">
+            {/* {achievements.map((achievement) => ( */}
+            {achievements.length === 0 ? (
+              <div>لا توجد إنجازات لعرضها.</div>
+            ) : (
+              achievements.map((achievement) => (
+              <div className="box" key={achievement.id}>
+                <img src={`/storage/${achievement.pic}`} alt="" />
+                <div className="overlay">
+                  <h3>{achievement.name}</h3>
+                  <p>{achievement.title}</p>
+                </div>
+              </div>
+                ))
+              )}
           </div>
         </div>
       </div>
     </div>
-    </div>
-    
-    </>
-  )
+  );
 }
 
 export default Achievement;
