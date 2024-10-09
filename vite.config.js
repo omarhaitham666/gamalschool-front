53f39bdb4380ce2ad25b8f16dcece52413d89358
mohamedElsayedIbrahim <mohamdeesayed@outlook.com>
2024-10-09 14:31:36 +0300

    fix base property in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/school-front/",
+  base:"/",//the base always is "/"
   server:{
     proxy:{
       '/api':{
